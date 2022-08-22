4b4b76e9bce82314071522256e74aae9eb03c4ec
hotrain81@gmail.com <hotrain81@gmail.com>
2022-08-22 13:44:28 +0800

    test

--- src/utils/configuration.js
+++ src/utils/configuration.js
@@ -5,7 +5,7 @@ export const CONTRACTADDRESS = {
 };
 
 // Set the price of your NFT
-export const PRICE = 0;
+export const PRICE = 1;
 
 // Set the max amount of your NFT collection
 export const MAX_AMOUNT = 1000;
